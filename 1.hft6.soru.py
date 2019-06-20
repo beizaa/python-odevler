8520be5ffbbf1b254bb0b93e9389570291831781
beizaa <51612975+beizaa@users.noreply.github.com>
2019-06-20 11:35:40 +0200

    Add files via upload

---
+++ 1.hft6.soru.py
@@ -0,0 +1,11 @@
+#verilen cumlenin ciktisini 3 sekilde alabiliriz.
+#1.uc tirnak isaretiyle;
+
+print("""Python’un kurucusu Guido Van Rossum, Hollanda’nin Amsterdam’daki  "Universiteit Van Amsterdam" okulundan  1982’de mezun olmuştur.""")
+
+#2.tek tirnak isaretiyle;
+
+print('Python’un kurucusu Guido Van Rossum, Hollanda’nin Amsterdam’daki  "Universiteit Van Amsterdam" okulundan  1982’de mezun olmuştur.')
+
+#3.ters slash isaretinin yardimiyla.
+print("Python’un kurucusu Guido Van Rossum, Hollanda’nin Amsterdam’daki  \"Universiteit Van Amsterdam\" okulundan  1982’de mezun olmuştur.")
